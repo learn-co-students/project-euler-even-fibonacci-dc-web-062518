3bc725be87b26a669c3a1343eb1ab653334d7cdf
github username <github email address>
2018-09-19 10:50:35 -0400

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,23 @@
 # Implement your procedural solution here!
+
+def sum_evens(arr)
+  sum = 0
+  arr.each do |num|
+    if num % 2 == 0
+      sum += num
+    end
+  end
+  sum
+end
+
+def even_fibonacci_sum(limit)
+  arr = [1, 1]
+  while arr.last < limit
+    var = arr.last + arr[arr.length - 2]
+    if var > limit
+      return sum_evens(arr)
+    else
+      arr << var
+    end
+  end
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,29 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum_evens(arr)
+    sum = 0
+    arr.each do |num|
+      if num % 2 == 0
+        sum += num
+      end
+    end
+    sum
+  end
+
+  def sum
+    arr = [1, 1]
+    while arr.last < @limit
+      var = arr.last + arr[arr.length - 2]
+      if var > @limit
+        return self.sum_evens(arr)
+      else
+        arr << var
+      end
+    end
+  end
+
+end
